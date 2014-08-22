f5aeaaf4da2169815679b9e8b30971979abb24f3
second-oliskin <olga.liskin@gmail.com>
2014-08-22 14:07:01 +0200

    first playable version of arithm3tics

---
+++ js/arithm3tics.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	createBoard();
+	
+	createWantedNumber();
+	
+});
+
+function createBoard(){
+	boardNumbers = createRandomBoardNumbers();
+	
+	for(i=0;i<8; i++){
+		var the_tr = $('<tr>');
+		for(j=0;j<8;j++){
+			var element = $('<div>').addClass('board-element-inner');
+			if(boardNumbers.length > 0){
+				element.text(boardNumbers[i*8+j]);
+			} else {
+				element.text(Math.floor(Math.random()*10));
+			}
+			var the_td = $('<td>').append($('<div>').addClass('board-element').append(element));
+			the_tr.append(the_td);
+		}
+		$("#board-table").append(the_tr);
+	}
+}
+
+function createRandomBoardNumbers(){
+	boardNumbers = new Array();
+	for(var i = 1; i<=8; i++){
+		for(var j = 1; j<= 8; j++){
+			boardNumbers.push(i);
+		}
+	}
+	return shuffleArray(boardNumbers);
+}
+
+function shuffleArray(array){
+	resultArray = new Array();
+	
+	while(array.length > 0){
+		var pickThisNumber = Math.floor(Math.random() * array.length);
+		resultArray.push(array[pickThisNumber]);
+		
+		array.splice(pickThisNumber,1);
+	}
+	return resultArray;
+
+}
+
+function createWantedNumber(){
+	$("#wanted-number").text(Math.floor(Math.random()*50 + 1));
+}
